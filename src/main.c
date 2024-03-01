4aa0cee48360b9f4b43d5a6e8676c10d249bc276
Guy Marshall <guymarshall9@gmail.com>
2024-03-01 12:23:31 +0000

    UPDATE: Add checks for argument count

--- src/main.c
+++ src/main.c
@@ -7,27 +7,54 @@ max length of name
 
 #include <stdio.h>
 
+void print_usage()
+{
+    printf("*** Usage ***\n");
+    printf("./main numberOfRandomNames minimumLengthOfName maximumLengthOfName\n");
+    printf("./main 1000 3 15\n");
+}
+
 int main(int argc, char *argv[])
 {
-    if (argc == 4)
+    int error = 0;
+    switch (argc)
     {
-        printf("Number of random names: %s\n", argv[1]);
-        printf("Min length of name: %s\n", argv[2]);
-        printf("Max length of name: %s\n", argv[3]);
-    }
-    else if (argc > 4)
-    {
-        printf("Too many arguments supplied.\n");
-    }
-    else
-    {
-        printf("Three argument expected.\n");
+        case 0:
+        case 1:
+            printf("No arguments provided.\n");
+            print_usage();
+            error = argc;
+            break;
+        case 2:
+            printf("Only number of random names to generate provided.\n");
+            print_usage();
+            error = argc;
+            break;
+        case 3:
+            printf("Only number of random names to generate and minimum length of name provided.\n");
+            print_usage();
+            error = argc;
+            break;
+        case 4:
+            // TODO: correct number of arguments provided
+            printf("Number of random names: %s\n", argv[1]);
+            printf("Min length of name: %s\n", argv[2]);
+            printf("Max length of name: %s\n", argv[3]);
+
+            // TODO: read first_names.txt into firstNames
+            // TODO: read last_names.txt into lastNames
+        default:
+            printf("Too many arguments provided.\n");
+            print_usage();
+            error = argc;
+            break;
     }
 
-    // TODO: check correct number of arguments
+    if (error > 0)
+    {
+        printf("An error has occurred.\n");
+        return error;
+    }
 
-    // TODO: read first_names.txt into firstNames
-
-    // TODO: read last_names.txt into lastNames
     return 0;
 }
